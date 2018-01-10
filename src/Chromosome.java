dd457455dc3e1d3285083819490328b214d95d10
Julian Lees <julian.j.lees@gmail.com>
2018-01-10 18:22:45 +1300

    Added recombination without mutation, mutation to be added next

--- src/Chromosome.java
+++ src/Chromosome.java
@@ -1,18 +1,53 @@
 import nz.ac.vuw.kol.OptimisationFunction;
 
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by Julian on 9/01/2018.
  */
 public class Chromosome {
 
+    private List<Integer> parentIndices = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4));
+
     private double[] values;
     private double fitness;
 
+    //Method to randomise Chromosome
     public Chromosome(double max, double min) {
         randomiseValues(max, min);
         calculateFitness();
+    }
+
+    //Method to create new chromosome using recombination
+    public Chromosome(Chromosome parent1, Chromosome parent2) {
+
+        values = new double [5];
+
+        //Generate random indices to select from each parent
+        //Generate parent 1 indices (non-duplicate)
+        for (int i = 0; i < 3; i++) {
+            int selectedIndex = (int) (Math.random() * parentIndices.size());
+            int p1Index = parentIndices.get(selectedIndex);
+            values[i] = parent1.getValues()[p1Index];
+            parentIndices.remove(selectedIndex);
+        }
+        //Generate parent 2 indices (non-duplicate)
+        resetParentIndicesList();
+        for (int i = 3; i < 5; i++) {
+            int selectedIndex = (int) (Math.random() * parentIndices.size());
+            int p2Index = parentIndices.get(selectedIndex);
+            values[i] = parent1.getValues()[p2Index];
+            parentIndices.remove(selectedIndex);
+        }
+
+        //Mutation to be added at a later stage
+
+        //Calculate chromosome fitness
+        calculateFitness();
+    }
+
+    public void resetParentIndicesList() {
+        parentIndices = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4));
     }
 
     public double[] getValues() {
--- src/EvolutionaryComputationAssessment.java
+++ src/EvolutionaryComputationAssessment.java
@@ -13,7 +13,7 @@ public class EvolutionaryComputationAssessment {
     public static void main(String[] args) {
 
         //Configuration variables
-        int numParents = 10;
+        int numGenerationZero = 10;
         int minGenerations = 10;
         int maxGenerations = 100;
         //Setup search space
@@ -24,17 +24,42 @@ public class EvolutionaryComputationAssessment {
 
         // 1. Initialisation phase: Generate the initial chromosome objects, and,
         // 2. Automatically evaluate fitness on creation
-        ArrayList<Chromosome> chromosomes = generateChromosomes(numParents, search_max, search_min);
+        List<Chromosome> chromosomes = generateChromosomes(numGenerationZero, search_max, search_min);
 
         // GENERATIONAL LOOP:
         System.out.println("Looping over minimum of " + minGenerations + " generations");
         int generation = 0;
 
         while (generation < minGenerations || generation < maxGenerations) {
-            // Sort ArrayList of chromosomes
-            Collections.sort(chromosomes,new FitnessComparator());
-            //Update minimum based on the chromosome with the smallest fitness function value
-            minimum = chromosomes.get(0);
+
+            minimum = sortAndUpdateMinimum(minimum, chromosomes);
+
+            for (int i = 0; i < 5; i++) {
+                System.out.println(chromosomes.get(i).getFitness());
+            }
+
+            //Select parents for next generation (the 'fittest' chromosomes in the List up to the number of chromosomes in generation zero)
+            System.out.println("Selecting fittest parents...");
+            chromosomes = chromosomes.subList(0, numGenerationZero / 2);
+
+            //Generate offspring from new parents
+            System.out.println("Generating offspring...");
+            int numParents = chromosomes.size();
+
+            for (int i = 0; i < numParents; i++) {
+
+                for (int j = 0; j < numParents; j++) {
+                    //If two different parents, recombine into new offspring and add to chromosomes List
+                    if (i != j) {
+
+                        Chromosome chromosome = new Chromosome(chromosomes.get(i), chromosomes.get(j));
+                        chromosomes.add(chromosome);
+
+                    }
+
+                }
+
+            }
 
             generation++;
 
@@ -43,6 +68,16 @@ public class EvolutionaryComputationAssessment {
         System.out.println("---- Minimum ----");
         minimum.printValues();
         minimum.printFitness();
+    }
+
+    public static Chromosome sortAndUpdateMinimum(Chromosome minimum, List<Chromosome> chromosomes) {
+        // Sort ArrayList of chromosomes
+        Collections.sort(chromosomes,new FitnessComparator());
+        //Update minimum based on the chromosome with the smallest fitness function value
+        if (chromosomes.get(0).getFitness() < minimum.getFitness()) {
+            minimum = chromosomes.get(0);
+        }
+        return minimum;
     }
 
     public static ArrayList<Chromosome> generateChromosomes(int numParents, double search_max, double search_min) {
